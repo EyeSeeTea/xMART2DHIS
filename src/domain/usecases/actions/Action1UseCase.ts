fd19ffed087ce10cb6a9bf1330272df22d539583
idelcano <ignaciodelcano@gmail.com>
2021-08-20 23:35:17 +0200

    refactor

--- src/domain/usecases/actions/Action1UseCase.ts
+++ src/domain/usecases/actions/Action1UseCase.ts
@@ -12,25 +12,9 @@ export class Action1UseCase implements UseCase {
         // TODO: Implement use-case logic
         // Use mart repository to get the data from xMART
         // Use instance repository to save the data to DHIS2
-        console.debug("Action 1", this.martRepository, this.instanceRepository);
 
-        this.getEvents();
-
-        /*         for (const row in mart_events){
-                    for (const specie in species){
-                        if (row == specie){
-                            console.debug("test")
-                        }
-                    }
-                }
-                console.debug("finish"); */
-        return Future.success(undefined);
-    }
-
-    private getEvents() {
         this.martRepository.listAll("FACT_MOLECULAR_TEST").run(
             options => {
-
                 const event = options?.map(item => {
                     return <ProgramEvent>{
                         //event: item["TEST_ID"],
@@ -40,83 +24,107 @@ export class Action1UseCase implements UseCase {
                         eventDate: item["Sys_CommitDateUtc"],
                         attributeOptionCombo: item["INSTITUTION_TYPE"],
                         programStage: "GeOxsjpEjSY",
-                        dataValues: [{
-                            dataElement: "yGY1TUqZsNf",
-                            value: item["SPECIES_CONTROL_FK__CODE"]
-                        }, {
-                            dataElement: "gXKPOItwUlb",
-                            value: item["SPECIES_FK__CODE"]
-                        }, {
-                            dataElement: "gqhSHmY7Etl",
-                            value: item["STAGE_ORIGIN_FK__CODE"]
-                        }, {
-                            dataElement: "sxLgkqTWM1c",
-                            value: item["YEAR_END"]
-                        }, {
-                            dataElement: "EvSWXtVdh6h",
-                            value: item["YEAR_START"]
-                        }, {
-                            dataElement: "nJGsnuqueOI",
-                            value: item["MONTH_END"]
-                        }, {
-                            dataElement: "aNTjTSnE4n3",
-                            value: item["MONTH_START"]
-                        }, {
-                            dataElement: "l7iRc4fVcRO",
-                            value: item["INSTITUTION_FK"]
-                        }, {
-                            dataElement: "Gwa8bf0Xh6Z",
-                            value: item["ADJ_MORTALITY_PERCENT_1X"]
-                        }, {
-                            dataElement: "Gwa8bf0Xh6Z",
-                            value: item["CITATION"]
-                        }, {
-                            dataElement: "l7iRc4fVcRO",
-                            value: item["INSECTICIDE_FK"]
-                        }, {
-                            dataElement: "FvbJ0tU5elQ",
-                            value: item["IR_STATUS_FK"]
-                        }, {
-                            dataElement: "NGU9TjLZcBg",
-                            value: item["METHOD_DETAILS"]
-                        }, {
-                            dataElement: "i4KoaSwzufn",
-                            value: item["MORTALITY_CONTROL"]
-                        }, {
-                            dataElement: "RosQioM91PZ",
-                            value: item["MORTALITY_NUMBER"]
-                        }, {
-                            dataElement: "d2yWBe9n1wr",
-                            value: item["MORTALITY_PERCENT"]
-                        }, {
-                            dataElement: "L42iFDUW1h7",
-                            value: item["NUMBER_MOSQ_CONTROL"]
-                        }, {
-                            dataElement: "FhshaqyCFQw",
-                            value: item["NUMBER_MOSQ_EXP"]
-                        }, {
-                            dataElement: "WJYxfzHrmQj",
-                            value: item["PUB_LINK"]
-                        }, {
-                            dataElement: "WJYxfzHrmQj",
-                            value: item["SPECIES_CONTROL_FK"]
-                        }, {
-                            dataElement: "WJYxfzHrmQj",
-                            value: item["PUB_LINK"]
-                        }, {
-                            dataElement: "v86CHHosXCi",
-                            value: item["TEST_TIME_FK"]
-                        }, {
-                            dataElement: "NGU9TjLZcBg",
-                            value: item["TEST_TYPE_FK"]
-                        }]
-
-                    }
+                        dataValues: [
+                            {
+                                dataElement: "yGY1TUqZsNf",
+                                value: item["SPECIES_CONTROL_FK__CODE"],
+                            },
+                            {
+                                dataElement: "gXKPOItwUlb",
+                                value: item["SPECIES_FK__CODE"],
+                            },
+                            {
+                                dataElement: "gqhSHmY7Etl",
+                                value: item["STAGE_ORIGIN_FK__CODE"],
+                            },
+                            {
+                                dataElement: "sxLgkqTWM1c",
+                                value: item["YEAR_END"],
+                            },
+                            {
+                                dataElement: "EvSWXtVdh6h",
+                                value: item["YEAR_START"],
+                            },
+                            {
+                                dataElement: "nJGsnuqueOI",
+                                value: item["MONTH_END"],
+                            },
+                            {
+                                dataElement: "aNTjTSnE4n3",
+                                value: item["MONTH_START"],
+                            },
+                            {
+                                dataElement: "l7iRc4fVcRO",
+                                value: item["INSTITUTION_FK"],
+                            },
+                            {
+                                dataElement: "Gwa8bf0Xh6Z",
+                                value: item["ADJ_MORTALITY_PERCENT_1X"],
+                            },
+                            {
+                                dataElement: "Gwa8bf0Xh6Z",
+                                value: item["CITATION"],
+                            },
+                            {
+                                dataElement: "l7iRc4fVcRO",
+                                value: item["INSECTICIDE_FK"],
+                            },
+                            {
+                                dataElement: "FvbJ0tU5elQ",
+                                value: item["IR_STATUS_FK"],
+                            },
+                            {
+                                dataElement: "NGU9TjLZcBg",
+                                value: item["METHOD_DETAILS"],
+                            },
+                            {
+                                dataElement: "i4KoaSwzufn",
+                                value: item["MORTALITY_CONTROL"],
+                            },
+                            {
+                                dataElement: "RosQioM91PZ",
+                                value: item["MORTALITY_NUMBER"],
+                            },
+                            {
+                                dataElement: "d2yWBe9n1wr",
+                                value: item["MORTALITY_PERCENT"],
+                            },
+                            {
+                                dataElement: "L42iFDUW1h7",
+                                value: item["NUMBER_MOSQ_CONTROL"],
+                            },
+                            {
+                                dataElement: "FhshaqyCFQw",
+                                value: item["NUMBER_MOSQ_EXP"],
+                            },
+                            {
+                                dataElement: "WJYxfzHrmQj",
+                                value: item["PUB_LINK"],
+                            },
+                            {
+                                dataElement: "WJYxfzHrmQj",
+                                value: item["SPECIES_CONTROL_FK"],
+                            },
+                            {
+                                dataElement: "WJYxfzHrmQj",
+                                value: item["PUB_LINK"],
+                            },
+                            {
+                                dataElement: "v86CHHosXCi",
+                                value: item["TEST_TIME_FK"],
+                            },
+                            {
+                                dataElement: "NGU9TjLZcBg",
+                                value: item["TEST_TYPE_FK"],
+                            },
+                        ],
+                    };
                 });
                 this.instanceRepository.postEvents(event);
             },
             error => console.debug(error)
         );
-    }
 
+        return Future.success(undefined);
+    }
 }
