e6b7c9bcfe9199d1c5786c3e460eeaaf4d8ea6e1
Alexis Rico <sferadev@gmail.com>
2021-11-24 08:15:56 +0100

    Clean-up error handling
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- src/data/repositories/AzureMSALRepository.ts
+++ src/data/repositories/AzureMSALRepository.ts
@@ -5,8 +5,13 @@ import { AzureRepository } from "../../domain/repositories/AzureRepository";
 const TENANT_ID = "f610c0b7-bd24-4b39-810b-3dc280afb590";
 const CLIENT_ID = "192e3789-682d-4aeb-8d05-1a70acb86ab8";
 
-const XMART_PROD_SCOPE = "api://712b0d0d-f9c5-4b7a-80d6-8a83ee014bca/.default";
-const XMART_UAT_SCOPE = "api://b85362d6-c259-490b-bd51-c0a730011bef/.default";
+const Scopes = {
+    XMART_PROD_SCOPE: "api://712b0d0d-f9c5-4b7a-80d6-8a83ee014bca/.default",
+    XMART_UAT_SCOPE: "api://b85362d6-c259-490b-bd51-c0a730011bef/.default",
+    XMART_PRIVATE_UAT_SCOPE: "api://a40621b1-fd6e-421b-bc44-c406b629e967/.default",
+    XMART_PRIVATE_DEV_SCOPE: "api://509fab01-ac48-4bdc-b1d6-6cdb4836d9c2/.default",
+    XMART_PRIVATE_PROD_SCOPE: "api://af17443e-8983-46ad-942d-e56b89ab44f2/.default",
+};
 
 export class AzureMSALRepository implements AzureRepository {
     public getInstance(): PublicClientApplication {
@@ -20,27 +25,32 @@ export class AzureMSALRepository implements AzureRepository {
     }
 
     public getTokenPROD(): FutureData<string> {
-        return this.getXmartToken(XMART_PROD_SCOPE);
+        return this.getXmartToken(Scopes.XMART_PROD_SCOPE);
     }
 
     public getTokenUAT(): FutureData<string> {
-        return this.getXmartToken(XMART_UAT_SCOPE);
+        return this.getXmartToken(Scopes.XMART_UAT_SCOPE);
     }
 
     private getXmartToken(scope: string): FutureData<string> {
         const client = this.getInstance();
-        const account = client.getAllAccounts()[0];
+        const [account] = client.getAllAccounts();
         if (!account) return Future.error("No active account");
 
-        const response = this.getInstance().acquireTokenSilent({
+        const request = {
             authenticationScheme: AuthenticationScheme.BEARER,
             scopes: [scope],
             account,
-        });
+        };
 
-        return Future.fromPromise(response).bimap(
-            ({ accessToken }) => accessToken,
-            error => String(error)
-        );
+        return Future.fromPromise(client.acquireTokenSilent(request))
+            .flatMapError(error => {
+                const redirect = Future.fromPromise(client.acquireTokenRedirect(request));
+                return redirect.flatMap(() => Future.error(error));
+            })
+            .bimap(
+                ({ accessToken }) => accessToken,
+                error => String(error)
+            );
     }
 }
